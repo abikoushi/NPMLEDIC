31de6c8ed94a1b6c88f410e1adfc896668e007b5
ko abe <koabe@konoMacBookpuro.todan1.kt.home.ne.jp>
2022-12-09 15:36:34 +0900

    fix man

--- R/simDIC.R
+++ R/simDIC.R
@@ -1,16 +1,22 @@
 #' @export simDIC
-simDIC <- function(y){
+simDIC <- function(y, at=NULL, WS=NULL, WE=NULL){
   N <- length(y)
-  at <- cumsum(rexp(N))
-  Sw <- rexp(N)
-  Ew <- rexp(N)
+  if(is.null(at)){
+    at <- cumsum(rexp(N))
+  }
+  if(is.null(WS)){
+    WS <- rexp(N)
+  }
+  if(is.null(WE)){
+    WE <- rexp(N)
+  }
   S <- at + y
   r_e <- runif(N)
   r_s <- runif(N)
-  LS <- S - r_s*Sw
-  RS <- S + (1-r_s)*Sw
-  LE <- at - r_e*Ew
-  RE <- at + (1-r_e)*Ew
+  LS <- S - r_s*WS
+  RS <- S + (1-r_s)*WS
+  LE <- at - r_e*WE
+  RE <- at + (1-r_e)*WE
   return(data.frame(LE=pmax(LE,0), RE=RE,
                     LS=pmax(LS,LE), RS=RS,
                     S=S, E=at))
